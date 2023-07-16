90e908e28e20f4412ddc3adca6c1033232a1dce8
PittsIrv <132793819+PittsIrv@users.noreply.github.com>
2023-07-16 15:21:48 +0800

    Update settings.py

--- user_management_app/user_management_project/settings.py
+++ user_management_app/user_management_project/settings.py
@@ -35,12 +35,12 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 Email config
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.163.com'
-EMAIL_PORT = 25
-EMAIL_HOST_USER = '13240161955@163.com'
-EMAIL_HOST_PASSWORD = 'PALSIHAUKMPYBPXC'
-EMAIL_USE_TLS = False
+ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+ EMAIL_HOST = 'smtp.163.com'
+ EMAIL_PORT = 25
+ EMAIL_HOST_USER = '13240161955@163.com'
+ EMAIL_HOST_PASSWORD = 'PALSIHAUKMPYBPXC'
+ EMAIL_USE_TLS = False
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 
