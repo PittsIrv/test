408cb51722601ecb2cf388fdec5e9777a1aee4a8
PittsIrv <132793819+PittsIrv@users.noreply.github.com>
2023-07-16 12:31:51 +0800

    Update urls.py

--- user_management_app/user_management_project/urls.py
+++ user_management_app/user_management_project/urls.py
@@ -32,6 +32,8 @@ urlpatterns = [
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('logout/', CustomLogoutView.as_view(), name='logout'),
+    path('user_profile/', views.user_profile, name='user_profile'),
+    path('password_change/', views.password_change, name='password_change'),
 ]
 
 
